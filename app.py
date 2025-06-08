6324f0960eda8f2008d2c9dff283efc2ffbe3641
Ekadashi <ekadashipradhan@gmail.com>
2025-06-08 13:56:18 -0400

    updated app

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_cached_portfolio_data():
 
 @app.route("/")
 def index():
-    growth_series, initial_value, latest_value, growth, years_held = get_cached_portfolio_data()
+    growth_series, initial_value, latest_value, growth, years_held, investment_scaled, target_growth_series = get_cached_portfolio_data()
     return render_template("index.html",
                            current_value=round(latest_value, 2),
                            growth_percent=round(growth, 2),
@@ -33,13 +33,17 @@ def index():
 
 @app.route("/plot.png")
 def plot_png():
-    growth_series, initial_value, latest_value, growth, years_held = get_cached_portfolio_data()
+    growth_series, initial_value, latest_value, growth, years_held, investment_scaled, target_growth_series = get_cached_portfolio_data()
+
 
     target_growth = 5.0 * years_held  # expressed as %
 
     fig, ax = plt.subplots(figsize=(10, 5))
     growth_series.plot(ax=ax, label="Growth (%)", linewidth=2)
-    ax.axhline(y=target_growth, color='red', linestyle='--', label='5% FD')
+    ax.bar(investment_scaled.index, investment_scaled.values,
+       width=1, alpha=0.3, color='orange', label='New Investment (scaled)')
+    #ax.axhline(y=target_growth, color='red', linestyle='--', label='5% FD')
+    target_growth_series.plot(ax=ax, color='red', linestyle='--', label='5% FD Target')
     ax.axhline(y=growth, color='cyan', linestyle='--', label=f'Growth: {growth:.2f}%')
 
     ax.set_xlabel("Date")
@@ -56,5 +60,3 @@ def plot_png():
 
 if __name__ == "__main__":
     app.run()
-
-
